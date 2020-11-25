656d5bd0d70b7d13ad481912d59da2dade3d15a9
szilvias <szilvia.sun@outlook.com>
2020-11-25 16:23:58 +0100

    parsing failures not fixed

--- coviddata.R
+++ coviddata.R
@@ -1,3 +1,11 @@
-print("the covideu project linked with github")
+# the covideu project linked with github
+library(readr)
+# import the data for bayern
+RKI_COVID19 <- read_csv("data/RKI_COVID19.csv") 
+summary(RKI_COVID19)
+# select the cases in Bayern
+bayerndata <- subset(RKI_COVID19, Bundesland == "Bayern")
+summary(bayerndata)
 
-print("week1")
+faelle_nach_datum <- read_csv("data/faelle_nach_datum.csv")
+summary(faelle_nach_datum)
